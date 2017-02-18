64c97fe6b46f79a4144f360146d18e60aac17047
av9746 <anze.vavken@gmail.com>
2017-02-18 09:53:10 +0100

    UI done problem with 4s

---
+++ PetApp/FancyView.swift
@@ -0,0 +1,17 @@
+//
+//  FancyView.swift
+//  PetApp
+//
+//  Created by Anze Vavken on 18/02/2017.
+//  Copyright © 2017 VavkenApps. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+    }
+
+}
