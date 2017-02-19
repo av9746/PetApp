866bf616b95d34333580a1b0be7ef4d499cfda7f
av9746 <anze.vavken@gmail.com>
2017-02-19 18:57:54 +0100

    uis done

---
+++ PetApp/ShadowView.swift
@@ -0,0 +1,24 @@
+//
+//  ShadowView.swift
+//  PetApp
+//
+//  Created by Anze Vavken on 19/02/2017.
+//  Copyright © 2017 VavkenApps. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(displayP3Red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 1.0).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+    }
+    
+
+}
--- PetApp/constants.swift
+++ PetApp/constants.swift
@@ -7,5 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 let KEY_UID = "uid"
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
